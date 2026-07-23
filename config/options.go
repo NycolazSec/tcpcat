19c8da43cf1e9b8339a06082b421850e98932a2d
NycolazSec <nicolas.blondellefr@gmail.com>
2026-07-23 23:08:36 +0200

    add options.go for configuration and command-line flag parsing

---
+++ config/options.go
@@ -0,0 +1,186 @@
+// config/options.go
+package config
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// ANSI Colors
+const (
+	Reset  = "\033[0m"
+	Bold   = "\033[1m"
+	Red    = "\033[91m"
+	Green  = "\033[92m"
+	Yellow = "\033[93m"
+	White  = "\033[97m"
+	Cyan   = "\033[96m"
+)
+
+var Banner = fmt.Sprintf(`
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀   ⠀⠀%s⢤⣶⣄%s⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀%s⣀⣤⡾⠿⢿⡀%s⠀⠀⠀⠀%s⣠⣶⣿⣷%s
+⠀⠀⠀⠀⠀⠀⠀⠀%s⢀⣴⣦⣴⣿⡋⠀⠀⠈⢳⡄%s⠀%s⢠⣾⣿⠁⠈⣿⡆%s      %s%stcpcat%s v5.0 %sby NycolazSec%s
+⠀⠀⠀⠀⠀⠀⠀%s⣰⣿⣿⠿⠛⠉⠉⠁⠀⠀⠀⠹⡄%s%s⣿⣿⣿⠀⠀⢹⇇%s      %s%sModular Security & Network Engine%s
+⠀⠀⠀⠀⠀%s⣠⣾⡿⠋⠁%s⠀⠀⠀⠀⠀⠀⠀⠀%s⣰⣏⢻⣿⣿⡆⠀⠸⣿%s
+⠀⠀⠀%s⢀⣴⠟⠁%s⠀⠀⠀⠀⠀⠀⠀⠀⠀%s⢠⣾⣿⣿⣆⠹⣿⣷⠀⢘⣿%s
+⠀⠀%s⢀⡾⠁%s⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀%s⢰⣿⣿⠋⠉⠛⠂⠹⠿⣲⣿⣿⣧%s
+⠀%s⢠⠏%s⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀%s⢀⣤⣿⣿⣿⣷⣾⣿⡇⢀⠀⣼⣿⣿⣿⣧%s
+%s⠰⠃%s⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀%s⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⡘⢿⣿⣿⣿%s
+%s⠁%s⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀%s⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⣷⡈⠿⢿⣿⡆%s
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀%s⠙⠛⠁⢙⠛⣿⣿⣿⣿⡟⠀⡿⠀⠀⢀⣿⡇%s
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀%s⠘⣶⣤⣉⣛⠻⠇⢠⣿⣾⣿⡄⢻⡇%s
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀%s⣿⣿⣿⣿⦦⣤⣾⣿⣿⣿⣿⣆%s
+`,
+	Red, Reset,
+	Red, Reset, White, Reset,
+	Red, Reset, White, Reset, White, Bold, Reset, Red, Reset,
+	Red, Reset, White, Reset, White, Bold, Reset,
+	Red, Reset, White, Reset,
+	Red, Reset, White, Reset,
+	Red, Reset, White, Reset,
+	Red, Reset, White, Reset,
+	Red, Reset, White, Reset,
+	Red, Reset, White, Reset,
+	White, Reset,
+	White, Reset,
+	White, Reset,
+)
+
+type Options struct {
+	// Targets
+	Target      string
+	InputFile   string
+	ExcludeHost string
+
+	// Host Discovery
+	PingScan      bool
+	SkipDiscovery bool
+	UdpPing       int
+	Traceroute    bool
+
+	// Scan Types
+	SynScan     bool
+	ConnectScan bool
+	AckScan     bool
+	UdpScan     bool
+
+	// Ports
+	Ports    string
+	TopPorts int
+
+	// Service & OS Detection
+	ServiceDetect bool
+	OsDetect      bool
+
+	// Evasion
+	SourcePort int
+	TTL        int
+	DataString string
+	DataHex    string
+
+	// Performance & Output
+	Timing     int
+	Workers    int
+	Verbose    bool
+	JsonOutput string
+}
+
+func ParseFlags() (*Options, error) {
+	opts := &Options{}
+
+	rawArgs := os.Args[1:]
+	var flagsArgs []string
+	var posArgs []string
+
+	// Liste des options recevant une valeur séparée par un espace
+	valueFlags := map[string]bool{
+		"-p":            true,
+		"-iL":           true,
+		"-j":            true,
+		"-w":            true,
+		"-T":            true,
+		"-g":            true,
+		"--ttl":         true,
+		"--data-string": true,
+		"--data":        true,
+		"--top-ports":   true,
+		"-PU":           true,
+	}
+
+	for i := 0; i < len(rawArgs); i++ {
+		arg := rawArgs[i]
+		if strings.HasPrefix(arg, "-") {
+			flagsArgs = append(flagsArgs, arg)
+			if valueFlags[arg] && i+1 < len(rawArgs) && !strings.HasPrefix(rawArgs[i+1], "-") {
+				flagsArgs = append(flagsArgs, rawArgs[i+1])
+				i++
+			}
+		} else {
+			posArgs = append(posArgs, arg)
+		}
+	}
+	os.Args = append([]string{os.Args[0]}, append(flagsArgs, posArgs...)...)
+
+	// Targets & Discovery
+	flag.StringVar(&opts.Ports, "p", "", "Port(s) to scan (e.g. 80 | 22,80,443 | 1-1000)")
+	flag.StringVar(&opts.InputFile, "iL", "", "Input target list from file")
+	flag.BoolVar(&opts.PingScan, "sn", false, "Ping Scan - disable port scan")
+	flag.BoolVar(&opts.SkipDiscovery, "Pn", false, "Treat all hosts as online")
+	flag.IntVar(&opts.UdpPing, "PU", 0, "UDP Ping discovery port")
+	flag.BoolVar(&opts.Traceroute, "traceroute", false, "Trace hop path to target")
+
+	// Scan Types
+	flag.BoolVar(&opts.AckScan, "sA", false, "ACK Scan (Firewall mapping)")
+	flag.BoolVar(&opts.SynScan, "sS", false, "SYN Stealth Scan")
+	flag.BoolVar(&opts.UdpScan, "sU", false, "UDP Port Scan")
+	flag.IntVar(&opts.TopPorts, "top-ports", 0, "Scan <number> most common ports")
+	flag.BoolVar(&opts.ServiceDetect, "sV", false, "Probe open ports for service/version info")
+
+	// Evasion flags
+	flag.IntVar(&opts.SourcePort, "g", 0, "Use given source port number")
+	flag.IntVar(&opts.TTL, "ttl", 0, "Set IP time-to-live field")
+	flag.StringVar(&opts.DataString, "data-string", "", "Append custom ASCII string to probes")
+	flag.StringVar(&opts.DataHex, "data", "", "Append custom hex string to probes")
+
+	// Timing & Output
+	flag.IntVar(&opts.Timing, "T", 3, "Set timing template (0-5, higher is faster)")
+	flag.IntVar(&opts.Workers, "w", 100, "Number of parallel workers")
+	flag.BoolVar(&opts.Verbose, "v", false, "Enable verbose output")
+	flag.StringVar(&opts.JsonOutput, "j", "", "Export results to JSON file")
+
+	flag.Usage = func() {
+		fmt.Println(Banner)
+		fmt.Printf("%sUsage:%s tcpcat <target> [options]\n\n", Bold, Reset)
+		fmt.Println(Cyan + "TARGET & DISCOVERY SPECIFICATION:" + Reset)
+		fmt.Printf("  %s<target>%s         Hostnames, IP addresses, CIDRs\n", Yellow, Reset)
+		fmt.Printf("  %s-sn%s             Ping Scan - disable port scan\n", Yellow, Reset)
+		fmt.Printf("  %s-Pn%s             Treat all hosts as online\n", Yellow, Reset)
+		fmt.Printf("  %s-PU <port>%s      UDP Ping discovery port\n", Yellow, Reset)
+		fmt.Println(Cyan + "\nPORT & SCAN SPECIFICATION:" + Reset)
+		fmt.Printf("  %s-p <ports>%s      Ports to scan (e.g. 80,443 | 1-1024)\n", Yellow, Reset)
+		fmt.Printf("  %s--top-ports <n>%s Scan n most common ports\n", Yellow, Reset)
+		fmt.Printf("  %s-sA%s             TCP ACK Scan (Firewall rules detection)\n", Yellow, Reset)
+		fmt.Printf("  %s-sV%s             Service & Version detection\n", Yellow, Reset)
+		fmt.Println(Cyan + "\nEVASION & OPTIONS:" + Reset)
+		fmt.Printf("  %s-g <port>%s       Use specified source port\n", Yellow, Reset)
+		fmt.Printf("  %s--ttl <val>%s     Set custom IP Time-To-Live\n", Yellow, Reset)
+		fmt.Printf("  %s--data-string%s   Append custom ASCII payload\n", Yellow, Reset)
+		fmt.Printf("  %s--traceroute%s    Trace hop path to target\n", Yellow, Reset)
+	}
+
+	flag.Parse()
+
+	if len(flag.Args()) > 0 {
+		opts.Target = flag.Arg(0)
+	}
+
+	if opts.Target == "" && opts.InputFile == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	return opts, nil
+}
